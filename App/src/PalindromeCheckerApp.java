ab044879bad2a175cc26344fdc0a07509ad5ae43
AGENT47MARINE <21kaushiknarayan@gmail.com>
2026-03-08 00:03:10 +0530

    Implement linked list based palindrome checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,12 +2,33 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.Deque;
-//version 7.0
+//version 8.0
 //author:AGENT47MARINE
-//Use Case 7: Deque-Based Optimized Palindrome Checker
+//Use Case 8: Linked List Based Palindrome Checker
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
+        static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    // Method to reverse linked list (UC8)
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node nextNode = null;
+        while (current != null) {
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
         System.out.println("Welcome to Palindrome Checker App Management System");
         String str = "madam";
 
@@ -104,6 +125,34 @@ public class PalindromeCheckerApp {
             }
         }
         System.out.println("UC7 (Deque Optimized): Is '" + inputUC7 + "' a palindrome? " + isPalUC7);
+        // UC8: Linked List Based Palindrome Checker
+        String inputUC8 = "madam";
+        Node head = new Node(inputUC8.charAt(0));
+        Node current = head;
+        for (int i = 1; i < inputUC8.length(); i++) {
+            current.next = new Node(inputUC8.charAt(i));
+            current = current.next;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverseList(slow);
+        Node firstHalf = head;
+        Node tempSecond = secondHalf;
+        boolean isPalUC8 = true;
+        while (tempSecond != null) {
+            if (firstHalf.data != tempSecond.data) {
+                isPalUC8 = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+        System.out.println("UC8 (Linked List): Is '" + inputUC8 + "' a palindrome? " + isPalUC8);
     }
+
 }
 
